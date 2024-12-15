3f84ce75459f845bf7e774d7d2fc37676ad52454
Mo3tassemdyab <moatassemdiab@gmail.com>
2024-12-15 13:24:24 +0200

    initial commit

---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./SideBar.module.css";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
+
+export default function SideBar() {
+  return (
+    <div className={styles.sidebar}>
+      {" "}
+      <Logo />
+      <AppNav />
+
+      <Outlet/>
+     
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+        Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
